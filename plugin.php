598f6b630813122001926a6fc478f5ae5ccd4e11
root <root@lamarck.fim.uni-erlangen.de>
2025-02-17 16:10:50 +0100

    contact data

--- plugin.php
+++ plugin.php
@@ -5,5 +5,5 @@ $id                  = "prvntdvcpp";                  // Plugin-ID
 $version             = "1.0.0";                       // Plugin-Version
 $ilias_min_version   = "7.0";                         // ILIAS 7 Mindestversion
 $ilias_max_version   = "7.999";                       // ILIAS 7 Maximalversion
-$responsible         = "Dein Name";
-$responsible_mail    = "deine.email@domain.tld";
+$responsible         = "André Schweigert";
+$responsible_mail    = "andre.schweigert@fau.de";
